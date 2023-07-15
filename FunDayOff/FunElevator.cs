e5f4ded61e794c131ea984bc79988f40ca5d0a7e
TextureTurtle <mraidanhall@gmail.com>
2023-07-14 21:36:16 -0400

    Doing the awesome

--- FunDayOff/FunElevator.cs
+++ FunDayOff/FunElevator.cs
@@ -6,29 +6,10 @@ namespace FunDayOff
 
     public class FunElevator : ModBehaviour
     {
-        private void Awake()
+        public override void Configure(IModConfig config)
         {
-            // You won't be able to access OWML's mod helper in Awake.
-            // So you probably don't want to do anything here.
-            // Use Start() instead.
-        }
-
-        private void Start()
-        {
-            // Starting here, you'll have access to OWML's mod helper.
-            ModHelper.Console.WriteLine($"My mod {nameof(FunDayOff)} is loaded!", MessageType.Success);
-
-            // Get the New Horizons API and load configs
-            var newHorizons = ModHelper.Interaction.TryGetModApi<INewHorizons>("xen.NewHorizons");
-            newHorizons.LoadConfigs(this);
-
-            // Example of accessing game code.
-            LoadManager.OnCompleteSceneLoad += (scene, loadScene) =>
-            {
-                if (loadScene != OWScene.SolarSystem) return;
-                ModHelper.Console.WriteLine("Loaded into solar system!", MessageType.Success);
-            };
+            var newFavorite = config.GetSettingsValue<string>("FUN Slider");
+            ModHelper.Console.WriteLine($"Elevator moved too: {newFavorite}!");
         }
     }
 }
-
